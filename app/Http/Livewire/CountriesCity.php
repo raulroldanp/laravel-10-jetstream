f43c8860cb002cdc003daa55295d334c127d5a14
raulroldanp <raulroldanp@gmail.com>
2023-04-09 21:59:03 +0200

    CountryCities component: mount and updatedCountry lifecycle hooks added

--- app/Http/Livewire/CountriesCity.php
+++ app/Http/Livewire/CountriesCity.php
@@ -3,11 +3,26 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Country;
+use App\Models\City;
 
 class CountriesCity extends Component
 {
+
+    public $countries, $cities, $country, $city;
+
+    public function mount() {
+        $this->countries = Country::all();
+        $this->cities = collect();
+    }
+
     public function render()
     {
         return view('livewire.countries-city');
+    }
+
+    public function updatedCountry($value) {
+        $this->cities = City::where('country_id', $value)->get();
+        $this->city = $this->cities->first()->id ?? null;
     }
 }
--- resources/views/livewire/countries-city.blade.php
+++ resources/views/livewire/countries-city.blade.php
@@ -10,7 +10,29 @@
                 Fill in the form. Choose the country, and cities list will be updated.
             </div>
 
-            
+            <form class="border-b-2 pb-10">
+                <div class="mt-4">
+                    <label for="country" class="block font-medium text-sm text-gray-700">Country*</label>
+                    <select wire:model='country' id="country" class="mt-2 text-sm sm:text-base pl-2 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" required>
+                        <option value="">-- choose country --</option>
+                        @foreach ($countries as $country)
+                            <option value="{{ $country->id }}">{{ $country->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="mt-4">
+                    <label for="city" class="block font-medium text-sm text-gray-700">City*</label>
+                    <select wire:model='city' id="city" class="mt-2 text-sm sm:text-base pl-2 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" required>
+                        @if ($cities->count() == 0)
+                            <option value="">-- choose country first --</option>
+                        @endif
+                        @foreach ($cities as $city)
+                            <option value="{{ $city->id }}">{{ $city->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </form>            
         </div>
 
     </div>
